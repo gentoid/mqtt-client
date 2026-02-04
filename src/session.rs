efe8a9efaa5eb6af6575054635fc5839274d44ce
Viktor Lazarev <taurus101v@gmail.com>
2026-02-04 23:24:34 +0100

    For in-flight pubs take QoS into account

--- src/session.rs
+++ src/session.rs
@@ -233,13 +233,25 @@ impl<'s, const N_PUB_IN: usize, const N_PUB_OUT: usize, const N_SUB: usize>
 }
 
 enum Kind {
-    Pub,
     Sub,
     Unsub,
 }
 
+#[derive(Clone, PartialEq)]
+enum PubInFlightState {
+    AwaitPubAck,
+    AwaitPubRec,
+    AwaitPubComp,
+}
+
+#[derive(Clone)]
+struct PubInFlight {
+    id: PacketId,
+    state: PubInFlightState,
+}
+
 struct PacketIdPool<const N_PUB_OUT: usize, const N_SUB: usize> {
-    in_flight_pub: [u16; N_PUB_OUT],
+    in_flight_pub: [Option<PubInFlight>; N_PUB_OUT],
     in_flight_sub: [u16; N_SUB],
     in_flight_unsub: [u16; N_SUB],
     next_id: u16,
@@ -248,7 +260,7 @@ struct PacketIdPool<const N_PUB_OUT: usize, const N_SUB: usize> {
 impl<const N_PUB_OUT: usize, const N_SUB: usize> PacketIdPool<N_PUB_OUT, N_SUB> {
     fn new() -> Self {
         Self {
-            in_flight_pub: [0u16; N_PUB_OUT],
+            in_flight_pub: [const { None }; N_PUB_OUT],
             in_flight_sub: [0u16; N_SUB],
             in_flight_unsub: [0u16; N_SUB],
             next_id: 1,
@@ -256,14 +268,31 @@ impl<const N_PUB_OUT: usize, const N_SUB: usize> PacketIdPool<N_PUB_OUT, N_SUB> 
     }
 
     fn clear(&mut self) {
-        self.in_flight_pub.fill(0);
+        self.in_flight_pub.fill(None);
         self.in_flight_sub.fill(0);
         self.in_flight_unsub.fill(0);
         self.next_id = 1;
     }
 
-    fn next_pub_id(&mut self) -> Result<PacketId, crate::Error> {
-        self.next_for(Kind::Pub)
+    fn next_pub_id(&mut self, qos: QoS) -> Result<PacketId, crate::Error> {
+        let index = self.in_flight_pub.iter().position(|p| p.is_none());
+
+        if index.is_none() {
+            return Err(crate::Error::NoPacketIdAvailable);
+        }
+
+        let id = PacketId(self.next_id()?);
+        let index = index.unwrap();
+
+        let state = match qos {
+            QoS::AtMostOnce => return Err(crate::Error::ProtocolViolation),
+            QoS::AtLeastOnce => PubInFlightState::AwaitPubAck,
+            QoS::ExactlyOnce => PubInFlightState::AwaitPubRec,
+        };
+
+        self.in_flight_pub[index] = Some(PubInFlight { id, state });
+
+        Ok(id)
     }
 
     fn next_sub_id(&mut self) -> Result<PacketId, crate::Error> {
@@ -309,8 +338,52 @@ impl<const N_PUB_OUT: usize, const N_SUB: usize> PacketIdPool<N_PUB_OUT, N_SUB> 
         Err(crate::Error::NoPacketIdAvailable)
     }
 
-    fn release_pub_id(&mut self, packet_id: &PacketId) -> Result<(), crate::Error> {
-        self.release_for(Kind::Pub, packet_id)
+    fn set_pubrel(&mut self, packet_id: &PacketId) -> Result<(), crate::Error> {
+        let index = self
+            .in_flight_pub
+            .iter()
+            .position(|p| p.as_ref().map(|p| &p.id) == Some(packet_id))
+            .ok_or(crate::Error::ProtocolViolation)?;
+
+        let publ = self.in_flight_pub[index]
+            .as_mut()
+            .ok_or(crate::Error::ProtocolViolation)?;
+
+        match publ.state {
+            PubInFlightState::AwaitPubRec => {
+                publ.state = PubInFlightState::AwaitPubComp;
+                Ok(())
+            }
+            PubInFlightState::AwaitPubComp => Ok(()),
+            _ => Err(crate::Error::ProtocolViolation),
+        }
+    }
+
+    fn release_pub_id(&mut self, packet_id: &PacketId, qos: QoS) -> Result<(), crate::Error> {
+        let compare_pub =
+            |publ: &Option<PubInFlight>| publ.as_ref().map(|p| p.id == *packet_id).unwrap_or(false);
+
+        match self.in_flight_pub.iter().position(compare_pub) {
+            Some(index) => {
+                let publ = self.in_flight_pub[index]
+                    .as_ref()
+                    .ok_or(crate::Error::ProtocolViolation)?;
+                let state = &publ.state;
+
+                if qos == QoS::AtLeastOnce && *state == PubInFlightState::AwaitPubAck {
+                    self.in_flight_pub[index] = None;
+                    return Ok(());
+                }
+
+                if qos == QoS::ExactlyOnce && *state == PubInFlightState::AwaitPubComp {
+                    self.in_flight_pub[index] = None;
+                    return Ok(());
+                }
+
+                Err(crate::Error::ProtocolViolation)
+            }
+            None => Err(crate::Error::ProtocolViolation),
+        }
     }
 
     fn release_sub_id(&mut self, packet_id: &PacketId) -> Result<(), crate::Error> {
@@ -335,7 +408,6 @@ impl<const N_PUB_OUT: usize, const N_SUB: usize> PacketIdPool<N_PUB_OUT, N_SUB> 
 
     fn array_mut(&mut self, kind: &Kind) -> &mut [u16] {
         match kind {
-            Kind::Pub => &mut self.in_flight_pub,
             Kind::Sub => &mut self.in_flight_sub,
             Kind::Unsub => &mut self.in_flight_unsub,
         }
@@ -343,7 +415,13 @@ impl<const N_PUB_OUT: usize, const N_SUB: usize> PacketIdPool<N_PUB_OUT, N_SUB> 
 
     #[inline]
     fn contains(&self, looking_for: u16) -> bool {
-        self.in_flight_pub.contains(&looking_for)
+        let compare_pub = |publ: &&Option<PubInFlight>| {
+            publ.as_ref()
+                .map(|p| p.id.0 == looking_for)
+                .unwrap_or(false)
+        };
+
+        self.in_flight_pub.iter().find(compare_pub).is_some()
             || self.in_flight_sub.contains(&looking_for)
             || self.in_flight_unsub.contains(&looking_for)
     }
